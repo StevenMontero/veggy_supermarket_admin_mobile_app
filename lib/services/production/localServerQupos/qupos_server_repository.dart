708dd2266decb8b689ea970a92320ab5bbfacf90
STEVEN GERARDO MONTERO MUNOZ <steven.monteromunoz@ucr.ac.cr>
2021-07-14 17:27:32 -0600

    auth en el url de qupos

--- lib/services/production/localServerQupos/qupos_server_repository.dart
+++ lib/services/production/localServerQupos/qupos_server_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:admin/models/product_api.dart';
 import 'package:dio/dio.dart';
 
@@ -19,11 +21,16 @@ class QuposRepository {
 
   Future<List<dynamic>> getProductsQuposRaw() async {
     // final response = await dioClient.get('201.207.180.249:45570/api/Articulos');
-    final response = await dioClient
-        .get('http://186.177.135.3:45570/api/Articulos?active=1');
+    String basicAuth = 'Basic ' + base64Encode(utf8.encode('pruebas:123456'));
+
+    final response =
+        await dioClient.get('http://186.177.135.3:45570/api/Articulos?active=1',
+            options: Options(headers: {
+              'authorization': basicAuth,
+            }));
 
     if (response.statusCode == 200) {
-      return response.data ;
+      return response.data;
     }
     return [];
   }
