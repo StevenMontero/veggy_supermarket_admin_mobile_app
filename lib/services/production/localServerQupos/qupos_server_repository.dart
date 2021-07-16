790c31fd6c0bfff490aff36e7ff906dbcbfbc87f
STEVEN GERARDO MONTERO MUNOZ <steven.monteromunoz@ucr.ac.cr>
2021-07-15 18:52:36 -0600

    usuario nuevo ya conectado a la base de datos de produccion

--- lib/services/production/localServerQupos/qupos_server_repository.dart
+++ lib/services/production/localServerQupos/qupos_server_repository.dart
@@ -21,7 +21,7 @@ class QuposRepository {
 
   Future<List<dynamic>> getProductsQuposRaw() async {
     // final response = await dioClient.get('201.207.180.249:45570/api/Articulos');
-    String basicAuth = 'Basic ' + base64Encode(utf8.encode('pruebas:123456'));
+    String basicAuth = 'Basic ' + base64Encode(utf8.encode('PROYECTO:12345'));
 
     final response =
         await dioClient.get('http://186.177.135.3:45570/api/Articulos?active=1',
